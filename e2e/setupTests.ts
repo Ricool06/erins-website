f96e54fff79ecbf6bcbcab9c93487ba18805c319
Ricool06 <Ricool06@users.noreply.github.com>
2020-05-17 18:37:02 +0100

    write e2e test for post display on blog page

--- e2e/setupTests.ts
+++ e2e/setupTests.ts
@@ -1,3 +1,6 @@
+import awsconfig from './aws-exports';
 import { browserPromise } from "./support";
+import { API } from "aws-amplify";
 
+beforeAll(() => API.configure(awsconfig));
 afterAll(async () => await (await browserPromise).close());
--- e2e/specs/write-post.spec.ts
+++ e2e/specs/write-post.spec.ts
@@ -1,6 +1,7 @@
 import awsconfig from '../aws-exports';
 import { cognitoProvider, signInUser, createUser, createBroswerContext, baseUrlPlus } from '../support';
 import { ChromiumBrowserContext } from 'playwright';
+import { randomBytes } from 'crypto';
 
 jest.setTimeout(30000);
 
@@ -13,7 +14,9 @@ describe('Login', () => {
     const user = await createUser();
     username = user.username;
     password = user.password;
+  });
 
+  beforeEach(async () => {
     ctx = await createBroswerContext();
   });
 
@@ -33,6 +36,57 @@ describe('Login', () => {
     const toast = await page.waitForSelector('div.ant-result-title');
 
     expect(await toast!.innerText()).toContain('Blog posted! 😘');
+  });
+
+  it('should prevent users that are not logged in from writing posts', async () => {
+    const page = await ctx.newPage();
+    await page.goto(baseUrlPlus('/new-post'));
+
+    await page.waitForSelector('#username');
+
+    const usernameField = await page.$('#username');
+    const passwordField = await page.$('#password');
+
+    await usernameField!.type('notvalid');
+    await passwordField!.type('wrong');
+
+    await page.keyboard.press('Enter');
+
+    const toast = await page.waitForSelector('div.toast');
+
+    expect(await toast.innerText()).toContain('User does not exist');
+  });
+
+  it.only('should show submitted posts on the front page', async () => {
+    const page = await ctx.newPage();
+    await page.goto(baseUrlPlus('/new-post'));
+    const uniqueSlug = randomBytes(8).toString('base64');
+
+    await signInUser(username, password, page);
+    const titleInput = await page.$('input');
+    const textArea = await page.$('textarea');
+    const postButton = await page.$('button.ant-btn-primary');
+
+    await titleInput!.type(`${uniqueSlug} first title!`);
+    await textArea!.type(`This is the first blog post!`);
+    await postButton!.click();
+
+    const okButton = await page.waitForSelector('div.ant-modal-confirm-btns > button');
+      
+    okButton.click();
+
+    await titleInput!.fill(`${uniqueSlug} second title!`);
+    await textArea!.fill('This is the second blog post!');
+    await postButton!.click();
+
+    await page.goto(baseUrlPlus('/'));
+
+    const cards = await page.$$('card');
+
+    const innerTexts = await Promise.all(cards.map(card => card.innerText()));
+
+    expect(innerTexts[0]).toContain(uniqueSlug);
+    expect(innerTexts[1]).toContain(uniqueSlug);
   });
 
   afterAll(async () => {
